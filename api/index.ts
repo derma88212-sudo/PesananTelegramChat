534732b63db08017636ef75b6048510e2676f61e
derma88212-sudo <derma88212@gmail.com>
2026-09-11 20:37:29 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import app from '../server';
-import { bootstrapServerless } from '../server';
+import app from '../server.js';
+import { bootstrapServerless } from '../server.js';
 
 // Vercel serverless function entrypoint.
 // On a cold start we run a one-time bootstrap (DB seeding + bot webhook setup)
